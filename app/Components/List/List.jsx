d79f93e9fb821ef6f87db4c4a8a369b9920673e1
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-01-23 18:51:25 +0600

    header

---
+++ app/Components/List/List.jsx
@@ -0,0 +1,9 @@
+const List = () => {
+    return ( 
+        <h1 className="text-2xl">
+            List
+        </h1>
+     );
+}
+ 
+export default List;
---
+++ app/Components/Map/Map.jsx
@@ -0,0 +1,9 @@
+const Map = () => {
+    return ( 
+        <h1 className="text-2xl">
+            Map
+        </h1>
+     );
+}
+ 
+export default Map;
